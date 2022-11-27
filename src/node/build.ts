991e628f297b94017c751347358b94bc94da2235
loogeek <xiaolong3956@gmail.com>
2022-11-28 00:24:38 +0800

    feat: add build and ssr

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,42 +1,84 @@
-import { build as viteBuild } from 'vite'
+import { build as viteBuild, InlineConfig } from 'vite'
+import {join} from 'path'
+import type {RollupOutput} from 'rollup'
+import * as fs from 'fs-extra'
+import pluginReact from '@vitejs/plugin-react'
+
 import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants/index';
 
 export async function bundle(root = process.cwd()) {
-  const clientBuild = async () => {
-    return viteBuild({
+  const resolveViteConfig = (isServer: boolean): InlineConfig => 
+    ({
       mode: 'production',
       root,
+      plugins: [pluginReact()],
       build: {
-        outDir: 'build',
+        ssr: isServer,
+        outDir: isServer ? '.temp' : 'build',
         rollupOptions: {
-          input: CLIENT_ENTRY_PATH,
+          input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
           output: {
-            format: 'esm'
+            format: isServer ? 'cjs' : 'esm'
           }
         }
       }
-    })
-  }
+    })  
 
-  const serverBuild = async () => {
-    return viteBuild({
-      mode: 'production',
-      root,
-      build: {
-        outDir: '.temp',
-        rollupOptions: {
-          input: SERVER_ENTRY_PATH,
-          output: {
-            format: 'cjs'
-          }
-        }
-      }
-    })
-  }
+  console.log(`Building client + server bundles...`);
 
-  await Promise.all([clientBuild(), serverBuild()]);
+  try {
+    const [clientBundle, serverBundle] = await Promise.all([
+      // client build
+      viteBuild(resolveViteConfig(false)), 
+      // server build
+      viteBuild(resolveViteConfig(true))]);
+  
+      return [clientBundle, serverBundle] as [RollupOutput, RollupOutput];
+      
+  } catch (error) {
+    console.error(error)    
+  }
 } 
 
+async function renderPage(
+  render: () => string,
+  root: string,
+  clientBundle: RollupOutput
+  ) {
+    const clientChunk = clientBundle.output.find(chunk => chunk.type === 'chunk' && chunk.isEntry)
+
+    console.log(`Rendering page in server side...`);
+    const appHtml = render()
+
+    const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    
+    <body>
+      <div id="root">${appHtml}</div>
+      <script type="module" src="/${clientChunk?.fileName}"></script>
+    </body>
+    
+    </html>
+    `
+  await fs.ensureDir(join(root, 'build'))
+  await fs.writeFile(join(root, 'build/index.html'), html)
+  await fs.remove(join(root, '.temp'))
+}
+
 export async function build(root: string) {
-  await bundle(root)
+  // 1. bundle - client 端 + server 端
+  const [clientBundle] = await bundle(root)
+  // 2. 加载ssr入口
+  const serverEntryPath = join(root, '.temp', 'ssr-entry.js') 
+  const { render } = require(serverEntryPath)
+  // 2. ssr -> html
+  await renderPage(render, root, clientBundle)
 }
