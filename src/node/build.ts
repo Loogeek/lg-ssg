05a4757c408755a6e0afdd1b8a6808536df04248
loogeek <xiaolong3956@gmail.com>
2022-12-03 17:18:21 +0800

    feat: add tsup

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,56 +1,58 @@
-import { build as viteBuild, InlineConfig } from 'vite'
-import {join} from 'path'
-import type {RollupOutput} from 'rollup'
-import * as fs from 'fs-extra'
-import pluginReact from '@vitejs/plugin-react'
+import { build as viteBuild, InlineConfig } from "vite";
+import { join } from "path";
+import type { RollupOutput } from "rollup";
+import fs from "fs-extra";
+import pluginReact from "@vitejs/plugin-react";
+import { pathToFileURL } from "url";
 
-import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants/index';
+import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from "./constants/index";
 
 export async function bundle(root = process.cwd()) {
-  const resolveViteConfig = (isServer: boolean): InlineConfig => 
-    ({
-      mode: 'production',
-      root,
-      plugins: [pluginReact()],
-      build: {
-        ssr: isServer,
-        outDir: isServer ? '.temp' : 'build',
-        rollupOptions: {
-          input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
-          output: {
-            format: isServer ? 'cjs' : 'esm'
-          }
-        }
-      }
-    })  
+  const resolveViteConfig = (isServer: boolean): InlineConfig => ({
+    mode: "production",
+    root,
+    plugins: [pluginReact()],
+    build: {
+      ssr: isServer,
+      outDir: isServer ? ".temp" : "build",
+      rollupOptions: {
+        input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
+        output: {
+          format: isServer ? "cjs" : "esm",
+        },
+      },
+    },
+  });
 
   console.log(`Building client + server bundles...`);
 
   try {
     const [clientBundle, serverBundle] = await Promise.all([
       // client build
-      viteBuild(resolveViteConfig(false)), 
+      viteBuild(resolveViteConfig(false)),
       // server build
-      viteBuild(resolveViteConfig(true))]);
-  
-      return [clientBundle, serverBundle] as [RollupOutput, RollupOutput];
-      
+      viteBuild(resolveViteConfig(true)),
+    ]);
+
+    return [clientBundle, serverBundle] as [RollupOutput, RollupOutput];
   } catch (error) {
-    console.error(error)    
+    console.error(error);
   }
-} 
+}
 
 async function renderPage(
   render: () => string,
   root: string,
   clientBundle: RollupOutput
-  ) {
-    const clientChunk = clientBundle.output.find(chunk => chunk.type === 'chunk' && chunk.isEntry)
+) {
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === "chunk" && chunk.isEntry
+  );
 
-    console.log(`Rendering page in server side...`);
-    const appHtml = render()
+  console.log(`Rendering page in server side...`);
+  const appHtml = render();
 
-    const html = `
+  const html = `
     <!DOCTYPE html>
     <html lang="en">
     
@@ -67,18 +69,18 @@ async function renderPage(
     </body>
     
     </html>
-    `
-  await fs.ensureDir(join(root, 'build'))
-  await fs.writeFile(join(root, 'build/index.html'), html)
-  await fs.remove(join(root, '.temp'))
+    `;
+  await fs.ensureDir(join(root, "build"));
+  await fs.writeFile(join(root, "build/index.html"), html);
+  await fs.remove(join(root, ".temp"));
 }
 
 export async function build(root: string) {
   // 1. bundle - client 端 + server 端
-  const [clientBundle] = await bundle(root)
+  const [clientBundle] = await bundle(root);
   // 2. 加载ssr入口
-  const serverEntryPath = join(root, '.temp', 'ssr-entry.js') 
-  const { render } = require(serverEntryPath)
+  const serverEntryPath = join(root, ".temp", "ssr-entry.js");
+  const { render } = await import(pathToFileURL(serverEntryPath).toString());
   // 2. ssr -> html
-  await renderPage(render, root, clientBundle)
+  await renderPage(render, root, clientBundle);
 }
--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -1,9 +1,19 @@
-import { join } from 'path'
+import { join } from "path";
 
-export const PACKAGE_PATH = join(__dirname, '..', '..', '..')
+export const PACKAGE_PATH = join(__dirname, "..");
 
-export const DEFAULT_HTML_PATH = join(PACKAGE_PATH, 'template.html')
+export const DEFAULT_HTML_PATH = join(PACKAGE_PATH, "template.html");
 
-export const CLIENT_ENTRY_PATH = join(PACKAGE_PATH, 'src', 'runtime', 'client-entry.tsx') 
+export const CLIENT_ENTRY_PATH = join(
+  PACKAGE_PATH,
+  "src",
+  "runtime",
+  "client-entry.tsx"
+);
 
-export const SERVER_ENTRY_PATH = join(PACKAGE_PATH, 'src', 'runtime', 'ssr-entry.tsx') 
+export const SERVER_ENTRY_PATH = join(
+  PACKAGE_PATH,
+  "src",
+  "runtime",
+  "ssr-entry.tsx"
+);
